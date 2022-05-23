1e45a67ba5b6ea225f6f4ad618d0124bafb1e212
samh <89654789+sayedAminul01@users.noreply.github.com>
2022-05-23 12:54:21 +0530

    Add files via upload

---
+++ simpleBlogApp/backend/app.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const res = require("express/lib/response");
+const app = express();
+const Post = require("./api/models/posts");
+const multer = require('multer');   // multer usage for uploading file on web.
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './uploads')
+    },
+    filename: function (req, file, cb) {
+      cb(null, `${file.fieldname}-${Date.now()}${getExt(file.mimetype)}`)
+    }
+})
+
+//   mimetype tell what type of file uploading.
+  const getExt = (mimeType) => {
+      switch(mimeType){
+        case "image/png":
+            return ".png";
+        case "image/jpeg":
+            return ".jpeg"
+      }
+  }
+
+
+
+const upload = multer({ storage: storage });
+const postsData = new Post();
+
+
+
+app.use(express.json());
+
+
+app.use((req, res, next)=> {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    next();
+});
+
+app.use('/uploads',express.static('uploads'));
+
+app.get("/api/posts", (req,res)=>{
+
+    res.status(200).send(postsData.get());
+});
+
+app.get("/api/posts/:post_id", (req,res)=> {
+    const postId = req.params.post_id;
+    const foundPost = postsData.getIndividualBlog(postId);
+    if(foundPost) {
+        res.status(200).send(foundPost)
+    }
+    else{
+        res.status(404).send("Not Found");
+    }
+});
+
+
+app.post("/api/posts", upload.single("post-image"), (req, res)=>{
+    const newPost = {
+        "id": `${Date.now()}`,
+        "title": req.body.title,
+        "content": req.body.content,
+        "post_image": req.file.path,
+        "added_date": `${Date.now()}`
+    }
+    postsData.add(newPost);
+    res.status(201).send("Ok");
+})
+
+app.listen(3000, ()=>console.log("Listening on http://localhost:3000"))
